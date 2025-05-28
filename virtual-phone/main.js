a07f5146ad5a2bf098d9c1e743a6b865dbad8d0f
felipemartinsbnf <felipemartinsbn@gmail.com>
2025-05-27 21:51:57 -0300

    feat: virtual-phone final

--- virtual-phone/main.js
+++ virtual-phone/main.js
@@ -1,7 +1,5 @@
 const display = document.querySelector('input[type="tel"]')
-
 const deleteIcon = document.querySelector('span[class="icon"]')
-
 const teclas = document.querySelectorAll('input[type="button"].btn-teclado')
 const teclasOperacao = document.querySelectorAll('input[type=button].btn-operador')
 
@@ -9,31 +7,41 @@ let operador1 = 0
 let operatorType = ""
 
 teclas.forEach((e) => {
-    e.addEventListener("click", (click) => {
+    e.addEventListener("click", () => {
         display.value += e.value
-    } )
+    })
 })
 
 teclasOperacao.forEach((e) => {
-
-    if(e.value == "=" ){
+    if (e.value === "=") {
         e.addEventListener("click", () => {
-            console.log(operatorType)
-            console.log(operador1.value)
-            console.log(display.value)
+            const operador2 = parseInt(display.value)
+
             switch (operatorType) {
-                case "+" : display.value = operador1 + parseInt(display.value)
-                case "-" : display.value = operador1 + parseInt(display.value)
-                case "×" : display.value = operador1 + parseInt(display.value)
-                case "÷" : display.value = operador1 + parseInt(display.value)
+                case "+":
+                    display.value = operador1 + operador2
+                    break
+                case "–":
+                    display.value = operador1 - operador2
+                    break
+                case "×":
+                    display.value = operador1 * operador2
+                    break
+                case "÷":
+                    display.value = operador1 / operador2
+                    break
+                default:
+                    display.value = "Erro"
             }
+
+            operador1 = 0
+            operatorType = ""
         })
     } else {
         e.addEventListener("click", () => {
-            operador1 =  parseFloat(display.value)
+            operador1 = parseInt(display.value)
             display.value = ""
-            operatorType = e.value 
-            console.log(operatorType)
+            operatorType = e.value
         })
     }
 })
